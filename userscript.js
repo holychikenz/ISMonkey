26b05024c2c0ba022ac8d09fb6f1499237f5ef50
Holychikenz <maskins@lbl.gov>
2021-01-26 15:12:38 -0800

    Initialize

---
+++ userscript.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         ISMonkey_Loader
+// @version      0.1.0
+// @description  ISMonkey Extension Loader 
+// @author       Holychikenz
+// @namespace    ISMonkey 
+// @updateURL    https://raw.githubusercontent.com/Holychikenz/ISMonkey/meta.js
+// @downloadURL  https://raw.githubusercontent.com/Holychikenz/ISMonkey/userscript.js
+// @match        http*://*idlescape.com/game
+// @run-at       document-end
+// ==/UserScript==
