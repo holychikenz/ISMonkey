b992eb4bdd11c60d0f4deb4b1fbb0bd5ebcbac64
Holychikenz <maskins@lbl.gov>
2021-01-26 18:26:26 -0800

    opened gone

--- extensions/spam.js
+++ extensions/spam.js
@@ -1,8 +1,8 @@
 class SpamExtension {
   constructor(){
     this.webpage = "<html><body></body></html>";
-    this.opened = window.open("");
-    this.webdom = this.opened.document.getElementsByTagName("BODY")[0];
+    this.owin = window.open("");
+    this.webdom = this.owin.document.getElementsByTagName("BODY")[0];
   }
   run(obj, msg) {
     this.webdom.innerHTML += `${JSON.stringify(msg)}<br/>`;
