266f4686553dc2448392e8d5b2235d4030396553
Holychikenz <maskins@lbl.gov>
2021-01-26 18:35:11 -0800

    proper update link

--- userscript.js
+++ userscript.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         ISMonkey_Loader
-// @version      0.1.4
+// @version      0.1.5
 // @description  ISMonkey Extension Loader 
 // @author       Holychikenz
 // @namespace    ISMonkey 
-// @updateURL    https://raw.githubusercontent.com/Holychikenz/ISMonkey/userscript.js
-// @downloadURL  https://raw.githubusercontent.com/Holychikenz/ISMonkey/userscript.js
+// @updateURL    https://raw.githubusercontent.com/holychikenz/ISMonkey/main/userscript.js
+// @downloadURL  https://raw.githubusercontent.com/holychikenz/ISMonkey/main/userscript.js
 // @match        https://www.idlescape.com/*
 // @require      https://raw.githubusercontent.com/holychikenz/ISMonkey/main/src/ISMonkey.js
 // @require      https://raw.githubusercontent.com/holychikenz/ISMonkey/main/extensions/spam.js
