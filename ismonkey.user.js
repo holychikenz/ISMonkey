cb4e60b9439118d2ca968861a7a40faf8142864c
Holychikenz <maskins@lbl.gov>
2021-01-29 17:49:08 -0800

    pushing new version

--- ismonkey.user.js
+++ ismonkey.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         ISMonkeyLoader
-// @version      0.1.15
+// @version      0.1.16
 // @description  ISMonkey Extension Loader
 // @author       Holychikenz
 // @namespace    ISMonkey
