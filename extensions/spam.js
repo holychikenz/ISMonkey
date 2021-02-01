d46b2cebca8a6b0869162038f9e7e4ad21618355
Holychikenz <maskins@lbl.gov>
2021-02-01 08:35:33 -0800

    Add spam filter

--- extensions/spam.js
+++ extensions/spam.js
@@ -3,11 +3,17 @@ class SpamExtension {
     // Reference the mother class enables the use of shared data
     this.monkey = monkey;
     this.options = options;
+    this.setup();
+  }
+  setup(){
+    this.filter = get(this.options, "filter", []);
     this.webpage = "<html><body></body></html>";
     this.owin = window.open("");
     this.webdom = this.owin.document.getElementsByTagName("BODY")[0];
   }
   run(obj, msg) {
-    this.webdom.innerHTML += `${JSON.stringify(msg)}<br/>`;
+    if( this.filter.includes(msg[0]) || this.filter.length == 0 ){
+      this.webdom.innerHTML += `${JSON.stringify(msg)}<br/>`;
+    }
   }
 }
