dd6a4f40aa945f7e07f7045cbdf848829eba6f4d
Holychikenz <maskins@lbl.gov>
2021-02-01 22:56:58 -0800

    Formatting time

--- src/ISMonkey.js
+++ src/ISMonkey.js
@@ -157,3 +157,13 @@ function effectiveLevel(skill){
 function sortObjectByKeys(o){
   return Object.keys(o).sort().reduce((r,k)=>(r[k] = o[k], r), {});
 }
+
+function timeFormat(time){
+  let hours = Math.floor(time/3600);
+  let minutes = Math.floor((time-(hours*3600))/60);
+  let seconds = time - (hours*3600) - (minutes*60);
+  if( hours < 10 ){hours = `0${hours}`;}
+  if( minutes < 10 ){minutes = `0${minutes}`;}
+  if( seconds < 10 ){seconds = `0${seconds}`;}
+  return `${hours}:${minutes}:${seconds}`;
+}
