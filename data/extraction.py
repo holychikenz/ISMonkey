eb9cfb45c120b312c259786441e0feeb8d77a52d
HighOnMikey <3639519+HighOnMikey@users.noreply.github.com>
2021-07-30 10:54:55 -0500

    Data extraction improvements (#7)
    * Format to make PEP 8 linters happy
    
    * Automatically detect the correct script for data extraction
    
    * Break out extractions into functions
    
    * Use single quotes for consistency
    
    * Try to use a more permanent regex look around to find items

--- data/extraction.py
+++ data/extraction.py
@@ -1,50 +1,107 @@
 import argparse
-import requests
 import json
+import logging
 import regex
+import requests
+
+logging.basicConfig(level=logging.INFO, format='[%(levelname)8s][%(filename)s:%(lineno)s - %(funcName)s()] %(message)s')
+idlescape_site = 'https://www.idlescape.com'
+default_main_chunk = f'{idlescape_site}/static/js/main.27754d83.chunk.js'
+fullDictlike = r'(\{)[\s\S]*(\})'
+elementDictlike = r'(?<rec>\{(?:[^{}]++|(?&rec))*\})'
+
 
 def pirates():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--url', default="https://www.idlescape.com/static/js/main.f5490297.chunk.js")
+    parser.add_argument('--url')
     return parser.parse_args()
 
-def main():
-    args = pirates()
-    dataFile = requests.get(args.url).text
-    enchantExpression = '(enchantments)[\s\S]*?(e.exports)'
-    itemExpression = '(kt\=)[\s\S]*?(yt\=)'
 
-    fullDictlike = '(\{)[\s\S]*(\})'
-    elementDictlike = '(?<rec>\{(?:[^{}]++|(?&rec))*\})'
-    # Get Enchants
-    x = regex.search(enchantExpression, dataFile).group(0)
-    # First break out the entire dictionary
-    fullEnchantDictlike = regex.search(fullDictlike, x).group(0)[1:-1]
-    enchantDict = {}
-    for x in regex.finditer(elementDictlike, fullEnchantDictlike):
-        xText = (x.group())[1:-1].split(',')
-        enchantID = xText[0].split(':')[1]
-        enchantName = xText[1].split(':')[1].replace('"','')
-        enchantDict[int(eval(enchantID))] = enchantName
-    # Save to file
-    with open('enchantments.json', 'w') as j:
-        json.dump( enchantDict, j, indent=2 )
+def fetch_data(args):
+    main_script = args.url
+    if not main_script:
+        logging.info('Automatically detecting main.<hex>.chunk.js')
+        main_script_re = r"main\.[a-zA-Z0-9]+\.chunk\.js"
+        idlescape_site_text = requests.get(idlescape_site).text
+        main_script_search = regex.search(main_script_re, idlescape_site_text)
+        if main_script_search is not None:
+            main_script = f'{idlescape_site}/static/js/{main_script_search.group(0)}'
+            logging.info(f'Detected {main_script}')
+        else:
+            main_script = default_main_chunk
+            logging.info('Main script not detected, using default fallback')
 
-    # Get items
-    x = regex.search(itemExpression, dataFile).group(0)
-    fullItemDictlike = regex.search(fullDictlike, x).group(0)[1:-1]
+    return requests.get(main_script).text
+
+
+def extract_items(data):
+    item_look_between_re = r'([a-zA-Z0-9_$]+)(?=\=\{1:\{id:1,name:"Gold").+?([a-zA-Z0-9_$]+)(?=\=function\([a-zA-Z0-9_$]+\))'
+    item_look_between = regex.search(item_look_between_re, data)
+
+    if len(item_look_between.groups()) != 2:
+        logging.error('Did not find suitable look between search terms, skipping item extraction')
+        return False
+
+    itemExpression = fr'({item_look_between.group(1)}\=)[\s\S]*?({item_look_between.group(2)}\=)'
+    x = regex.search(itemExpression, data).group(0)
+    try:
+        fullItemDictlike = regex.search(fullDictlike, x).group(0)[1:-1]
+    except AttributeError:
+        logging.error('Did not find the proper set of items')
+        return False
+
+    logging.info('Extracting items')
     itemDict = {}
     for x in regex.finditer(elementDictlike, fullItemDictlike):
         xText = (x.group())[1:-1].split(',')
         try:
             itemID = [xt.split(':')[1] for xt in xText if xt.split(':')[0] == 'id'][0]
-            itemName = [xt.split(':')[1] for xt in xText if xt.split(':')[0] == 'name'][0].replace('"','')
+            itemName = [xt.split(':')[1] for xt in xText if xt.split(':')[0] == 'name'][0].replace('"', '')
             itemDict[int(eval(itemID))] = itemName
         except:
-            pass # Not an item
-    # Save to file
-    with open('items.json', 'w') as j:
-        json.dump( itemDict, j, indent=2 )
+            pass  # Not an item
+
+    return itemDict
+
+
+def extract_enchantments(data):
+    enchantExpression = r'(enchantments)[\s\S]*?(e.exports)'
+    x = regex.search(enchantExpression, data).group(0)
+
+    # First break out the entire dictionary
+    try:
+        fullEnchantDictlike = regex.search(fullDictlike, x).group(0)[1:-1]
+    except AttributeError:
+        logging.error('Did not find the proper set of enchantments')
+        return False
+
+    logging.info('Extracting enchantments')
+    enchantDict = {}
+    for x in regex.finditer(elementDictlike, fullEnchantDictlike):
+        xText = (x.group())[1:-1].split(',')
+        enchantID = xText[0].split(':')[1]
+        enchantName = xText[1].split(':')[1].replace('"', '')
+        enchantDict[int(eval(enchantID))] = enchantName
+
+    return enchantDict
+
+
+def main():
+    args = pirates()
+    dataFile = fetch_data(args)
+
+    enchantments = extract_enchantments(dataFile)
+    if enchantments:
+        with open('enchantments.json', 'w') as j:
+            json.dump(enchantments, j, indent=2)
+        logging.info('Wrote enchantments.json')
+
+    items = extract_items(dataFile)
+    if items:
+        with open('items.json', 'w') as j:
+            json.dump(items, j, indent=2)
+        logging.info('Wrote items.json')
+
 
 if __name__ == '__main__':
     main()
