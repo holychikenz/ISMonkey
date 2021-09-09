f018959521ea5d32356d3942e91d69701a903068
Holychikenz <maskins@lbl.gov>
2021-09-09 08:44:44 -0700

    Using Schlermbo socket listener

--- singles/smithing.user.js
+++ singles/smithing.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         ISMonkeyLoader: Smithing 
-// @version      0.4.2
+// @version      0.4.3
 // @description  ISMonkey Extension Loader
 // @author       Holychikenz
 // @namespace    ISMonkey
