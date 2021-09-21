bba4d4c4dcfc96dde29a5570c17906d41da83a9f
Holychikenz <maskins@lbl.gov>
2021-09-21 07:14:03 -0700

    Versioning and removal of old single files

--- ismonkey.user.js
+++ ismonkey.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         ISMonkeyLoader
-// @version      0.4.5
+// @version      0.4.6
 // @description  ISMonkey Extension Loader
 // @author       Holychikenz
 // @namespace    ISMonkey
