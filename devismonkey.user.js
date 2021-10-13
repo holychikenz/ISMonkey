4ea579ec58c9ab0544465f9a7dcb469693ab33e7
holychikenz <76458314+holychikenz@users.noreply.github.com>
2021-10-12 17:08:53 -0700

    Hotfix for lootvalue (#21)
    * hotfix: reset loot value

--- devismonkey.user.js
+++ devismonkey.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Dev_ISMonkeyLoader
-// @version      0.4.7
+// @version      0.4.8
 // @description  ISMonkey Extension Loader Dev Branch
 // @author       Holychikenz
 // @namespace    ISMonkey
