2f57a733bb4649475610c17435ce7872f8b38c84
holychikenz <76458314+holychikenz@users.noreply.github.com>
2021-10-19 10:33:45 -0700

    Dungeoneering > auto-eat shown, savedata added (#26)
    - Auto-eat shown in combat area (also mutation observer on that area available)
    - Savedata button added (could be useful)
    - Dodge chance added

--- ismonkey.user.js
+++ ismonkey.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         ISMonkeyLoader
-// @version      0.5.2
+// @version      0.5.3
 // @description  ISMonkey Extension Loader
 // @author       Holychikenz
 // @namespace    ISMonkey
