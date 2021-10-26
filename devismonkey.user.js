6014ce67a15206fbf9ae7361054cd240a9e53613
Holychikenz <maskins@lbl.gov>
2021-10-26 16:37:51 -0700

    patch 0.5.6

--- devismonkey.user.js
+++ devismonkey.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Dev_ISMonkeyLoader
-// @version      0.5.5
+// @version      0.5.6
 // @description  ISMonkey Extension Loader Dev Branch
 // @author       Holychikenz
 // @namespace    ISMonkey
