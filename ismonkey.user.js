3af1f4a796da80115e45227538f99f2b20557af2
holychikenz <76458314+holychikenz@users.noreply.github.com>
2021-10-26 16:38:16 -0700

    patch 0.5.6 (#29)
    * Converting pixel width to percent. Works better with resizing

--- ismonkey.user.js
+++ ismonkey.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         ISMonkeyLoader
-// @version      0.5.5
+// @version      0.5.6
 // @description  ISMonkey Extension Loader
 // @author       Holychikenz
 // @namespace    ISMonkey
