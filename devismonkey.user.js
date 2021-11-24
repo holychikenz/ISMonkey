29c464089b7a5ae39b961b030ea4320a49595785
platinumscott <vision@austingulati.com>
2021-11-24 11:49:58 -0500

    Initial commit

--- devismonkey.user.js
+++ devismonkey.user.js
@@ -4,20 +4,22 @@
 // @description  ISMonkey Extension Loader Dev Branch
 // @author       Holychikenz
 // @namespace    ISMonkey
-// @updateURL    https://raw.githubusercontent.com/holychikenz/ISMonkey/dev/devismonkey.user.js
-// @downloadURL  https://raw.githubusercontent.com/holychikenz/ISMonkey/dev/devismonkey.user.js
+// @updateURL    https://raw.githubusercontent.com/platinumscott/ISMonkey/dev/devismonkey.user.js
+// @downloadURL  https://raw.githubusercontent.com/platinumscott/ISMonkey/dev/devismonkey.user.js
 // @match        *://*.idlescape.com/*
+// @require      https://raw.githubusercontent.com/janl/mustache.js/v4.2.0/mustache.js
 // @require      https://raw.githubusercontent.com/HighOnMikey/idlescape-socketio-listener/main/src/idlescape-listener.js
-// @require      https://raw.githubusercontent.com/holychikenz/ISMonkey/dev/src/ISMonkey.js
-// @require      https://raw.githubusercontent.com/holychikenz/ISMonkey/dev/extensions/playerdata.js
-// @require      https://raw.githubusercontent.com/holychikenz/ISMonkey/dev/extensions/foodinfo.js
-// @require      https://raw.githubusercontent.com/holychikenz/ISMonkey/dev/extensions/dungeoneering.js
-// @require      https://raw.githubusercontent.com/holychikenz/ISMonkey/dev/extensions/loottracking.js
-// @require      https://raw.githubusercontent.com/holychikenz/ISMonkey/dev/extensions/injectcss.js
-// @require      https://raw.githubusercontent.com/holychikenz/ISMonkey/dev/extensions/jiggyslide.js
-// @require      https://raw.githubusercontent.com/holychikenz/ISMonkey/dev/extensions/animationcancel.js
-// @require      https://raw.githubusercontent.com/holychikenz/ISMonkey/dev/extensions/runecrafting.js
-// @require      https://raw.githubusercontent.com/holychikenz/ISMonkey/dev/extensions/smithing.js
+// @require      https://raw.githubusercontent.com/platinumscott/ISMonkey/dev/src/ISMonkey.js
+// @require      https://raw.githubusercontent.com/platinumscott/ISMonkey/dev/extensions/playerdata.js
+// @require      https://raw.githubusercontent.com/platinumscott/ISMonkey/dev/extensions/foodinfo.js
+// @require      https://raw.githubusercontent.com/platinumscott/ISMonkey/dev/extensions/dungeoneering.js
+// @require      https://raw.githubusercontent.com/platinumscott/ISMonkey/dev/extensions/loottracking.js
+// @require      https://raw.githubusercontent.com/platinumscott/ISMonkey/dev/extensions/injectcss.js
+// @require      https://raw.githubusercontent.com/platinumscott/ISMonkey/dev/extensions/jiggyslide.js
+// @require      https://raw.githubusercontent.com/platinumscott/ISMonkey/dev/extensions/animationcancel.js
+// @require      https://raw.githubusercontent.com/platinumscott/ISMonkey/dev/extensions/runecrafting.js
+// @require      https://raw.githubusercontent.com/platinumscott/ISMonkey/dev/extensions/smithing.js
+// @require      https://raw.githubusercontent.com/platinumscott/ISMonkey/dev/extensions/statistics.js
 // @run-at       document-start
 // @grant        none
 // ==/UserScript==
@@ -32,6 +34,7 @@
   // Socket Listeners
   ismonkey.addSocketExtension( Dungeoneering, options );
   ismonkey.addSocketExtension( LootTracking, options );
+  ismonkey.addSocketExtension( Statistics, options );
   // Mutation Observers / Async Extensions
   ismonkey.addAsyncExtension( FoodInfo, options );
   ismonkey.addAsyncExtension( InjectCSS, options );
