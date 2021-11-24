7b0e8cbfd2f3bd3a4ecd070967a41f3ee908c355
austin <82424146+platinumscott@users.noreply.github.com>
2021-11-24 14:31:43 -0500

    import from windows

--- extensions/statistics.js
+++ extensions/statistics.js
@@ -1,11 +1,74 @@
 class Statistics {
-  
+
   constructor (monkey, options) {
     this.monkey = monkey;
     this.options = options;
     this.data = {};
+    this.otherTabs = [];
+    this.tab = null;
+    this.panel = null;
+
+    this.zoneIDs = [
+      [12, 'Farm'],
+      [23, 'Caves'],
+      [11, 'City'],
+      [13, 'Lava Maze'],
+      [28, 'Corrupted Lands'],
+      [27, 'Valley of Giants'],
+      [29, 'Chaos Wastes'],
+    ];
+
+    this.monsters = {
+      1: { name: 'Chicken', image: 'chicken.png' },
+    };
 
     this.insertStatisticsTab();
+  }
+
+  getMonsterName(monsterID) {
+    if (monsterID in this.monsters) {
+      return this.monsters[monsterID].name;
+    }
+    return monsterID;
+  }
+
+  getMonsterImage(monsterID) {
+    if (monsterID in this.monsters) {
+      return this.monsters[monsterID].image;
+    }
+    return monsterID + '.png';
+  }
+
+  addCSS () {
+    var style = document.createElement('style');
+    style.innerHTML=
+      `
+      .statistics-panel-inner {
+        display: grid;
+        grid-template-columns: 50% 50%;
+      }
+
+      .statistics-grid-item {
+        padding: 25px;
+      }
+
+      .statistics-grid-item strong {
+        font-size: 125%;
+      }
+
+      .statistics-monsters {
+        display: grid;
+      }
+
+      .statistics-grid-item .img-wrapper {
+        padding-bottom: 10px;
+      }
+
+      .statistics-grid-item img {
+        width: 50px;
+      }
+      `
+    document.body.appendChild(style)
   }
 
   run (obj, msg) {
@@ -19,52 +82,95 @@ class Statistics {
     }
   }
 
+  // TODO: Move these to some utility fn file.
+  selectTab(el) {
+    el.classList.add('selected-tab');
+  }
+
+  deselectTab(el) {
+    el.classList.remove('selected-tab');
+  }
+
+  hide(el) {
+    if (el) el.style.display = 'none';
+  }
+
+  show(el) {
+    if (el) el.style.display = '';
+  }
+
   insertStatisticsTab (promise) {
     let self = this;
     promise = promise || new Promise( ()=>{} );
-    if (document.getElementsByClassName('nav-tab-container').length == 0) {
+    if (document.getElementsByClassName('game-right-panel').length == 0) {
       setTimeout(function() { self.insertStatisticsTab(promise) }, 1000);
       return false;
     } else {
       promise.then();
     }
 
-    let tab = document.createElement('div');
-    tab.className = 'nav-tab noselect'
+    self.addCSS();
+    self.otherTabs = document.querySelectorAll('.game-right-panel .nav-tab-container div');
+
+    self.tab = document.createElement('div');
+    self.tab.className = 'nav-tab noselect'
 
     let icon = document.createElement('img');
     icon.className = 'nav-tab-icon icon-border';
     icon.src = '/images/ui/hiscore_icon.png';
 
-    tab.append(icon);
-    tab.innerHTML += 'Statistics';
+    self.tab.append(icon);
+    self.tab.innerHTML += 'Statistics';
 
-    let container = document.getElementsByClassName('nav-drawer-container')[0];
-    container.append(tab);
+    let container = document.getElementsByClassName('game-right-panel')[0].children[0];
+    container.append(self.tab);
 
-    tab.addEventListener('click', () => self.drawStatisticsPanel(self) );
+    self.tab.addEventListener('click', () => self.drawStatisticsPanel(self) );
+    self.otherTabs.forEach((el) => {
+      el.addEventListener('click', () => {
+        self.deselectTab(self.tab);
+        self.hide(self.panel);
+      })
+    });
   }
 
   drawStatisticsPanel (self) {
-    let container = document.getElementsByClassName('right-panel-content')[0];
-    container.clear();
+    self.otherTabs.forEach(self.deselectTab);
+    self.selectTab(self.tab);
 
-    let panel = document.createElement('div');
-    panel.className = 'statistics-panel';
-    container.append(panel);
+    let otherPanels = document.querySelectorAll('.inventory-panel, .combat-gear-container, .game-right-panel-social');
+    otherPanels.forEach(self.hide);
 
-    panel.innerHTML = 'test123';
+    if (!self.panel) {
+      self.panel = document.createElement('div');
+      self.panel.className = 'statistics-panel';
 
-    function resetMenu(e) {
-      if (!e.target.classList.contains('monkey')){
-        panel.remove();
-        document.removeEventListener('click', resetMenu);
+      let inner = document.createElement('div');
+      inner.className = 'statistics-panel-inner';
+      self.panel.append(inner);
+
+      let container = document.getElementsByClassName('right-panel-content')[0];
+      container.append(self.panel);
+
+      for (let i in self.zoneIDs) {
+        let zoneID = self.zoneIDs[i][0];
+        let zoneName = self.zoneIDs[i][1];
+        if (zoneID in self.data.data.monsterKills) {
+          var kills = self.data.data.monsterKills[zoneID];
+          var gridItem = document.createElement('div');
+          gridItem.className = 'statistics-grid-item';
+          gridItem.innerHTML = '<strong> ' + zoneName + '</strong><div class="statistics-monsters">';
+          for (let monsterID in kills) {
+              let name = self.getMonsterName(monsterID);
+              let image = self.getMonsterImage(monsterID);
+              gridItem.innerHTML += '<div class="img-wrapper"><img src="https://idlescape.com/images/combat/monsters/' + image + '" alt="' + name + '"></div>' + kills[monsterID];
+          }
+          gridItem.innerHTML += '</div>'
+          inner.append(gridItem);
+        }
       }
     }
-    let listener = document.addEventListener('click', resetMenu);
-  }
 
-  fillSettingsDom(self, dom){
-    dom.innerHTML="test";
+    self.show(self.panel);
   }
 }
