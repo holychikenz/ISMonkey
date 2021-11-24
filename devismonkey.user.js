f4c8f2fd5c155a681af4172ed6878ed84a043bfc
austin <82424146+platinumscott@users.noreply.github.com>
2021-11-24 14:36:51 -0500

    Update devismonkey.user.js

--- devismonkey.user.js
+++ devismonkey.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Dev_ISMonkeyLoader
-// @version      0.5.25
+// @version      0.5.26
 // @description  ISMonkey Extension Loader Dev Branch
 // @author       Holychikenz
 // @namespace    ISMonkey
