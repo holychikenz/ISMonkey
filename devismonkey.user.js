7d9d0d71b5b6594a5ee5a281ac0918c67b30e237
platinumscott <vision@austingulati.com>
2021-11-24 12:29:36 -0500

    basic

--- devismonkey.user.js
+++ devismonkey.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Dev_ISMonkeyLoader
-// @version      0.5.24
+// @version      0.5.25
 // @description  ISMonkey Extension Loader Dev Branch
 // @author       Holychikenz
 // @namespace    ISMonkey
