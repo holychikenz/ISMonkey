a11affb74e2710c1e2ca381495018a3cfd187274
austin <82424146+platinumscott@users.noreply.github.com>
2021-11-24 14:36:39 -0500

    Update statistics.js

--- extensions/statistics.js
+++ extensions/statistics.js
@@ -7,6 +7,7 @@ class Statistics {
     this.otherTabs = [];
     this.tab = null;
     this.panel = null;
+    this.fmt = new Intl.NumberFormat();
 
     this.zoneIDs = [
       [12, 'Farm'],
@@ -20,6 +21,22 @@ class Statistics {
 
     this.monsters = {
       1: { name: 'Chicken', image: 'chicken.png' },
+      2: { name: 'Cow', image: 'cow.png' },
+      3: { name: 'Goblin', image: 'goblin.png' },
+      4: { name: 'Rat', image: 'rat.png' },
+      7: { name: 'Imp/Greater Imp', image: 'imp.png' },
+      5: { name: 'Guard', image: 'guard.svg' },
+      10: { name: 'Black Knight', image: 'black_knight.png' },
+      9: { name: 'Lesser Demon', image: 'lesser_demon_no_highlight.png' },
+      8: { name: 'Deadly Red Spider', image: 'deadly_red_spider.png' },
+      20: { name: 'Bone Giant', image: 'bone_giant.png' },
+      21: { name: 'Infected Naga', image: 'infected_naga.png' },
+      22: { name: 'Corrupted Tree', image: 'corrupted_tree.png' },
+      13: { name: 'Fire Giant', image: 'fire_giant.png' },
+      14: { name: 'Moss Giant', image: 'moss_giant.png' },
+      15: { name: 'Ice Giant', image: 'ice_giant.png' },
+      23: { name: 'Chaos Giant', image: 'chaos_giant.png' },
+      34: { name: 'Chaotic Abomination', image: 'chaotic_abomination.png' },
     };
 
     this.insertStatisticsTab();
@@ -46,10 +63,11 @@ class Statistics {
       .statistics-panel-inner {
         display: grid;
         grid-template-columns: 50% 50%;
+        padding: 15px;
       }
 
       .statistics-grid-item {
-        padding: 25px;
+        padding: 10px;
       }
 
       .statistics-grid-item strong {
@@ -58,14 +76,23 @@ class Statistics {
 
       .statistics-monsters {
         display: grid;
+        grid-template-columns: 25% 25% 25% 25%;
+      }
+
+      .statistics-monsters > div {
+        width: 75px;
+        text-align: center;
+        padding: 5px 0;
       }
 
       .statistics-grid-item .img-wrapper {
-        padding-bottom: 10px;
+        padding: 10px 12px;
+        height: 75px;
       }
 
       .statistics-grid-item img {
-        width: 50px;
+        max-width: 50px;
+        max-height: 50px;
       }
       `
     document.body.appendChild(style)
