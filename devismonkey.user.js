f9328407058ca69aceaeff22bb1e30341bfab30a
holychikenz <76458314+holychikenz@users.noreply.github.com>
2022-01-28 11:09:34 -0800

    version 0.6.9 (#65)

--- devismonkey.user.js
+++ devismonkey.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Dev_ISMonkeyLoader
-// @version      0.6.8
+// @version      0.6.9
 // @description  ISMonkey Extension Loader Dev Branch
 // @author       Holychikenz
 // @namespace    ISMonkey
--- meta.js
+++ meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         ISMonkey_Loader
-// @version      0.6.8
+// @version      0.6.9
 // @description  ISMonkey Extension Loader 
 // @author       Holychikenz
 // @namespace    ISMonkey 
