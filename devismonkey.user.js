f3c283ae784b5c2a43f6391b74cfdef0858987c3
Holychikenz <maskins@lbl.gov>
2022-02-10 10:45:24 -0800

    Fixed issue with channel icons getting pulled into name when clicked

--- devismonkey.user.js
+++ devismonkey.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Dev_ISMonkeyLoader
-// @version      0.7.1
+// @version      0.7.2
 // @description  ISMonkey Extension Loader Dev Branch
 // @author       Holychikenz
 // @namespace    ISMonkey
